f797cf3a34e786c46560ac6c1bb2063ef22a23be
yuheng_dai <yuheng_dai@intsig.net>
2023-05-05 18:41:23 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    //设置别名
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  plugins: [vue()],
+  server: {
+    port: 8080,
+    hmr: {
+      host: '127.0.0.1',
+      port: 8080
+    },
+    //设置https代理
+    
+  }
+})
